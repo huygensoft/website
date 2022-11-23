9f6f98473f0edd3ce98f9e5a38b6f2c8b1c2f493
Hestia-Arc <starafolab@gmail.com>
2022-11-23 18:32:22 +0100

    Add Cookie Banner

--- public/src/app.js
+++ public/src/app.js
@@ -3,3 +3,19 @@ window.onload = function() {
     let date = new Date().getFullYear();
     document.getElementById("footer-year").innerHTML = date;
 }
+
+
+//Cookies Banner
+const cookieContainer = document.querySelector(".cookie-banner-container");
+const cookieButton = document.querySelector(".cookie-button");
+
+cookieButton.addEventListener("click", () => {
+    cookieContainer.classList.remove("active");
+});
+
+setTimeout( () => {
+    cookieContainer.classList.add("active");
+}, 3000);
+
+//Set cookie
+let cookieName = "Huygensoft";
