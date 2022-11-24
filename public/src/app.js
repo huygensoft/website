9cc87816fc342ed28bc78722c1727d5e95e85fcd
Hestia-Arc <starafolab@gmail.com>
2022-11-25 00:17:50 +0100

    Update cookie

--- public/src/app.js
+++ public/src/app.js
@@ -6,16 +6,43 @@ window.onload = function() {
 
 
 //Cookies Banner
-const cookieContainer = document.querySelector(".cookie-banner-container");
+const cookieContainer = document.querySelector("#cookie");
 const cookieButton = document.querySelector(".cookie-button");
 
+// Set cookie
+setCookie = (cName, cValue, expDays) => {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+
+// Get cookie 
+getCookie = (cName) => {
+    const name  = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArr = cDecoded.split("; ");
+    let value;
+    cArr.forEach(val => {
+        if(val.indexOf(name) === 0) value = val.substring(name.length);
+    })
+
+    return value;
+}
+
+
+// Cookie Button clicked
 cookieButton.addEventListener("click", () => {
-    cookieContainer.classList.remove("active");
+    cookieContainer.style.display = "none";
+
+    setCookie("cookie", true, 30);
 });
 
-setTimeout( () => {
-    cookieContainer.classList.add("active");
-}, 3000);
 
-//Set cookie
-let cookieName = "Huygensoft";
+// 
+cookieMessage = () => {
+    if(!getCookie("cookie")) 
+    document.querySelector("#cookie").style.display = "block";
+}
+
+window.addEventListener("load", cookieMessage);
