9dca81b9f8829f4be6122ddfbe0d4e8a240ef10f
Afolabi Esther <starafolab@gmail.com>
2023-03-24 21:17:58 +0100

    Add contact us page

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,25 @@
+// Mobile Navigation
+// Hamburger starts
+const hamburger = document.getElementById("bar-container");
+const menu = document.querySelector(".nav-menu");
+const menuLink = document.querySelectorAll(".menu-link");
+
+hamburger.addEventListener("click", showMenu);
+
+function showMenu() {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+menuLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}
+// Hamburger ends
+
+
+
 // Footer date
 window.onload = function() {
     let date = new Date().getFullYear();
